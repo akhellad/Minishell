3040842a92eb44c0baf48cd6e1881f034ff87500
Ahmed Khelladi <akhellad@z3r5p1.42lyon.fr>
2023-07-24 00:54:17 +0200

    update

--- srcs/redirs.c
+++ srcs/redirs.c
@@ -6,7 +6,7 @@
 /*   By: akhellad <akhellad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 21:42:38 by akhellad          #+#    #+#             */
-/*   Updated: 2023/07/23 21:58:43 by akhellad         ###   ########.fr       */
+/*   Updated: 2023/07/24 00:52:47 by akhellad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,22 @@ int     add_new_redirs(t_lexer *tmp, t_parser_infos *parser_infos)
 	return (0);
 }
 
-void    sort_redirs(t_parser_infos *parser_infos)
+void	sort_redirs(t_parser_infos *parser_infos)
 {
-    t_lexer *tmp;
+	t_lexer	*tmp;
 
-    tmp = parser_infos->lexers;
-    while (tmp && tmp->token == 0)
-        tmp = tmp->next;
-    if(!tmp || tmp->token == PIPE)
-        return ;
-    if (!tmp->next)
-        parser_error(0, parser_infos->infos, parser_infos->infos->lexers);
-    if (tmp->next->token)
-        double_token_error(parser_infos->infos, parser_infos->lexers, tmp->next->token);
-    if ((tmp->token >= GREAT && tmp->token <= TWO_LESS))
+	tmp = parser_infos->lexers;
+	while (tmp && tmp->token == 0)
+		tmp = tmp->next;
+	if (!tmp || tmp->token == PIPE)
+		return ;
+	if (!tmp->next)
+		parser_error(0, parser_infos->infos, parser_infos->lexers);
+	if (tmp->next->token)
+		double_token_error(parser_infos->infos,
+			parser_infos->lexers, tmp->next->token);
+	if ((tmp->token >= GREAT
+			&& tmp->token <= TWO_LESS))
 		add_new_redirs(tmp, parser_infos);
 	sort_redirs(parser_infos);
 }
